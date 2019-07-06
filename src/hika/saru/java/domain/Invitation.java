716d907964a9f737cb17e268cd1445fc9ee3e62e
saru lim <saru.lim@mcngamehub.com>
2019-07-06 20:20:18 +0900

    Initial commit

---
+++ src/hika/saru/java/domain/Invitation.java
@@ -0,0 +1,8 @@
+package hika.saru.java.domain;
+
+public class Invitation {
+    public static final Invitation EMPTY = new Invitation(null);
+    private final Theater theater;
+
+    public Invitation(Theater theater) {this.theater = theater;}
+}
